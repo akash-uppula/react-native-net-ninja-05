e1c03dcf89f34da90dcbd9e049f8a073fa9f0fe4
akash-uppula <akashuppula02@gmail.com>
2026-08-31 11:33:38 +0530

    14 done

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
